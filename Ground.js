35d4198928aaa2764f93b13225e6470a751256b2
hiteshi15 <74048103+hiteshi15@users.noreply.github.com>
2020-12-16 21:03:42 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,15 @@
+class Ground {
+    constructor() {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(450, 400, 500, 20, options);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, 500, 20);
+    }
+  };
